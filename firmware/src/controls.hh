56d55fbcd593d7dfe5ec2ef98d7950bf9f42ab9f
Dan Green <danngreen1@gmail.com>
2026-02-10 17:11:51 -0800

    Start IR Capture class

--- firmware/src/controls.hh
+++ firmware/src/controls.hh
@@ -2,6 +2,7 @@
 #include "conf/board_conf.hh"
 #include "debug.hh"
 #include "drivers/adc_builtin.hh"
+#include "ir_capture.hh"
 #include "util/filter.hh"
 #include <cstdint>
 
@@ -9,13 +10,13 @@ namespace RemoteVolume
 {
 
 class Controls {
-public:
 	static inline std::array<uint16_t, 1> adc_buffer;
-
 	mdrivlib::AdcDmaPeriph<Board::AdcConf> adc{adc_buffer, Board::AdcChans};
-
 	Oversampler<64, uint16_t> smooth_adc;
 
+	IRCapture ir;
+
+public:
 	Controls() = default;
 
 	uint16_t raw_adc();
@@ -26,9 +27,6 @@ public:
 		adc.register_callback([this] { smooth_adc.add_val(adc_buffer[0]); });
 
 		adc.start();
-	}
-
-	void update() {
 	}
 };
 } // namespace RemoteVolume
---
+++ firmware/src/ir_capture.hh
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace RemoteVolume
+{
+
+struct IRCapture {};
+
+} // namespace RemoteVolume
