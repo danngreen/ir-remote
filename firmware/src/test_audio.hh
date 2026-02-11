f7852c6216a892b7004988caf332cedc367d4144
Dan Green <danngreen1@gmail.com>
2026-02-10 17:05:28 -0800

    Make test audio class (not in use, but available to test on home system)

--- firmware/src/test_audio.hh
+++ firmware/src/test_audio.hh
@@ -1,36 +1,34 @@
 #pragma once
 #include "audio_stream_conf.hh"
-#include "params.hh"
 #include "util/zip.hh"
 
 // Testing
 #include "util/oscs.hh"
 
-namespace LDKit
+namespace IRREmote
 {
+
 class TestAudio {
-	Params &params;
+	float &vol;
 	TriangleOscillator<48000> osc1{1000};
 	TriangleOscillator<48000> osc2{15000};
 
 public:
-	TestAudio(Params &params)
-		: params{params} {}
+	TestAudio(float &vol)
+		: vol{vol} {
+	}
 
 	void update(const AudioStreamConf::AudioInBlock &in, AudioStreamConf::AudioOutBlock &out) {
-		if (params.delay_feed > 0.5f) {
-			for (auto [o, i] : zip(out, in)) {
-				o.chan[0] = i.chan[0];
-				o.chan[1] = i.chan[1];
-			}
-		} else {
-			osc1.set_frequency(params.time * 100.f);
-			osc2.set_frequency(params.time * 1000.f);
-			for (auto [o, i] : zip(out, in)) {
-				o.chan[0] = (osc1.process_float() - 0.5f) * 0x00FFFFFFUL;
-				o.chan[1] = (osc2.process_float() - 0.5f) * 0x00FFFFFFUL;
-			}
+		osc1.set_frequency(vol * 100.f + 100.f);
+		osc2.set_frequency(vol * 1000.f + 100.f);
+		for (auto [o, i] : zip(out, in)) {
+			o.chan[0] = (osc1.process_float() - 0.5f) * 0x00FFFFFFUL;
+			o.chan[1] = (osc2.process_float() - 0.5f) * 0x00FFFFFFUL;
 		}
 	}
+	// out.chan[0] = sinf(2.f * (float)M_PI * x) * 0x7F0000;
+	// x += 1.f / 480.f;
+	// if (x > 1.f)
+	// 	x -= 1.f;
 };
-} // namespace LDKit
+} // namespace IRREmote
